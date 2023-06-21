87ae6d175a36174e68321e9a6d80f3a8f922b76a
VEL <dr.vel145676@mail.ru>
2023-06-22 01:48:00 +0500

    feat: update function in util.js and data.js

--- js/util.js
+++ js/util.js
@@ -1,4 +1,4 @@
-// Функция взята из источника и доработана: https://learnjs.ru/lessons/math-random/?ysclid=lj19va18kc123768048
+
 
 function getRandomPositiveNumber(a, b) {
   const min = Math.ceil(Math.min(Math.abs(a), Math.abs(b)));
@@ -7,29 +7,28 @@ function getRandomPositiveNumber(a, b) {
   return result;
 }
 
+function createRandomIdFromRangeGenerator (min, max) {
+  const previousValues = [];
+
+  return function () {
+    let currentValue = getRandomPositiveNumber(min, max);
+    if (previousValues.length >= (max - min + 1)) {
+      return null;
+    }
+    while (previousValues.includes(currentValue)) {
+      currentValue = getRandomPositiveNumber(min, max);
+    }
+    previousValues.push(currentValue);
+    return currentValue;
+  };
+}
+
+const getRandomArrayElement = (elements) => elements[getRandomPositiveNumber(0, elements.length - 1)];
+
 function checkStringLength (string, length) {
   return string.length <= length;
 }
 
 checkStringLength(30, 50);
 
-
-//Функция взята с сайта: https://otus.ru/journal/generiruem-sluchajnye-chisla-v-javascript/?ysclid=lj4bo1r368990993308
-//Возвращает случайное уникальное число
-const getArrayOfUniqueNumbers = function (upper) {
-  const numReserve = [];
-  while (numReserve.length < upper) {
-    const randomNumber = getRandomPositiveNumber(1, upper);
-    let found = false;
-    for (let i=0; i < numReserve.length; i++) {
-      if (numReserve[i] === randomNumber) {
-        found = true;
-        break;
-      }
-    }
-    if (!found) {numReserve[numReserve.length]=randomNumber;}
-  }
-  return numReserve;
-};
-
-export {getRandomPositiveNumber, getArrayOfUniqueNumbers};
+export {getRandomPositiveNumber, createRandomIdFromRangeGenerator, getRandomArrayElement, checkStringLength};
