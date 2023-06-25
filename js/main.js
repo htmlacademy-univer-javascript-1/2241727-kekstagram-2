76438ce65fcbe47c7224186d39abc426286defe8
VEL <dr.vel145676@mail.ru>
2023-06-25 20:48:51 +0500

    complete task11

--- js/main.js
+++ js/main.js
@@ -1,5 +1,22 @@
-import './data.js';
-import './miniatures.js';
-import './big-picture.js';
-import   './api.js';
-import   './form.js';
+import {getMiniatures} from './api.js';
+import {insertPhotoMiniature} from './miniatures.js';
+import {showFileForm} from './form.js';
+import {showFilters} from './filters.js';
+
+const fileInput = document.querySelector('#upload-file');
+const preview = document.querySelector('.img-upload__preview').querySelector('img');
+
+getMiniatures()
+  .then((r) => {
+    insertPhotoMiniature(r);
+    showFilters(r);
+  });
+
+fileInput.addEventListener('change', () => {
+  const file = fileInput.files[0];
+  if (!['png', 'jpg', 'jpeg'].some((suf) => file.name.toLowerCase().endsWith(suf))){
+    return;
+  }
+  preview.src = URL.createObjectURL(file);
+  showFileForm();
+});
